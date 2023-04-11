a160532fe04924603152184006554632005b9e73
Washington Soares <washingtonudi@gmail.com>
2023-04-11 07:25:13 -0300

    Add 10 sec API delay

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function Layout() {
 
   React.useEffect(() => {
     fetch(
-      "https://hub.dummyapis.com/delay?seconds=5"
+      "https://hub.dummyapis.com/delay?seconds=10" // arbitrary number
     )
       .then((res) => res.text())
       .then(data => {
